592452ac1cf930fcb3e3fdc3d088f5f12964e669
Daljit Singh Dhillon <ZEAC037@live.rhul.ac.uk>
2024-02-08 11:41:10 +0000

    Feat: extractForm and validateTitleLink methods now consider asset type.
    validateTitleLink is now called validateTitleTypeLink to match it's new
    considerations.
    
    extractForm now requests the type field from the corresponding HTML
    file.

--- src/main/java/com/team05/assetsrepo/FormController.java
+++ src/main/java/com/team05/assetsrepo/FormController.java
@@ -16,150 +16,159 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Controller responsible for extracting and processing data entered into the
- * asset creation form.
+ * Controller responsible for extracting and processing data entered into the asset creation form.
  */
 @Controller
 public class FormController {
 
-	private final NamedParameterJdbcTemplate jdbcTemplate;
+  private final NamedParameterJdbcTemplate jdbcTemplate;
 
-	private HashSet<String> programmingLanguagesSet;
+  private HashSet<String> programmingLanguagesSet;
 
-	/**
-	 * Injecting the JdbcTemplate registered in the Spring appliction context by the
-	 * DBConfig class.
-	 */
-	public FormController(NamedParameterJdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-		setProgLangSet();
-	}
+  /**
+   * Injecting the JdbcTemplate registered in the Spring appliction context by the DBConfig class.
+   */
+  public FormController(NamedParameterJdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+    setProgLangSet();
+  }
 
-	/**
-	 * Setting up the hashset for the different possible programming languages.
-	 */
-	public void setProgLangSet() {
-		programmingLanguagesSet = new HashSet<String>();
-		programmingLanguagesSet.add("Java");
-		programmingLanguagesSet.add("JavaScript");
-		programmingLanguagesSet.add("C++");
-		programmingLanguagesSet.add("Other");
-	}
+  /**
+   * Setting up the hashset for the different possible programming languages.
+   */
+  public void setProgLangSet() {
+    programmingLanguagesSet = new HashSet<String>();
+    programmingLanguagesSet.add("Java");
+    programmingLanguagesSet.add("JavaScript");
+    programmingLanguagesSet.add("C++");
+    programmingLanguagesSet.add("Other");
+  }
 
-	/**
-	 * Adds new programming languages as possible valid selections.
-	 * 
-	 * @param value - The programming language to be added to the set.
-	 */
-	public void addProgLangSet(String value) {
-		programmingLanguagesSet.add(value);
-	}
+  /**
+   * Adds new programming languages as possible valid selections.
+   * 
+   * @param value - The programming language to be added to the set.
+   */
+  public void addProgLangSet(String value) {
+    programmingLanguagesSet.add(value);
+  }
 
-	/**
-	 * The extractForm method handles POST requests to the "/submit" URL.
-	 * RequestParam annotations are used to extract necessary form parameters /
-	 * attributes.
-	 *
-	 * @return returns a HTTP "Created" response, indicating that a resource has
-	 *         been successfully created.
-	 * @throws InvalidSelection 
-	 */
-	@PostMapping("/submit")
-	public String extractForm(@RequestParam String title, @RequestParam int lines,
-			@RequestParam String link, @RequestParam String lang, @RequestParam String assoc,
-			@RequestParam String date, Model model) throws InvalidSelection {
+  /**
+   * The extractForm method handles POST requests to the "/submit" URL. RequestParam annotations are
+   * used to extract necessary form parameters / attributes.
+   *
+   * @return returns a HTTP "Created" response, indicating that a resource has been successfully
+   *         created.
+   * @throws InvalidSelection
+   */
+  @PostMapping("/submit")
+  public String extractForm(@RequestParam String title, @RequestParam String type,
+      @RequestParam int lines, @RequestParam String link, @RequestParam String lang,
+      @RequestParam String assoc, @RequestParam String date, Model model) throws InvalidSelection {
 
-		String message = validateTitleLink(title, link, lang, assoc);
-		model.addAttribute("error", message);
-		validateSelection(lang);
+    String message = validateTitleTypeLink(title, type, link, lang, assoc);
+    model.addAttribute("error", message);
+    validateSelection(lang);
 
-		insertAssetData(title, lines, link, lang, assoc, date);
+    insertAssetData(title, lines, link, lang, assoc, date);
 
-		return "submit";
+    return "submit";
 
-	}
+  }
 
-	/**
-	 * Inserts a row into the "std_assets" table in the database representing the
-	 * asset.
-	 *
-	 * @param title the asset's title.
-	 * @param lines the number of lines of the asset.
-	 * @param link  the link to the asset.
-	 * @param lang  the programming language the asset is written in.
-	 * @param assoc the asset's association(s).
-	 * @param date  the asset's creation date.
-	 */
-	public void insertAssetData(String title, int lines, String link, String lang, String assoc, String date) {
+  /**
+   * Inserts a row into the "std_assets" table in the database representing the asset.
+   *
+   * @param title the asset's title.
+   * @param lines the number of lines of the asset.
+   * @param link the link to the asset.
+   * @param lang the programming language the asset is written in.
+   * @param assoc the asset's association(s).
+   * @param date the asset's creation date.
+   */
+  public void insertAssetData(String title, int lines, String link, String lang, String assoc,
+      String date) {
 
-		String statement = "INSERT INTO std_assets (title, lines, link, lang, assoc, date) "
-				+ "VALUES (:title, :lines, :link, :lang, :assoc, :date)";
+    String statement = "INSERT INTO std_assets (title, lines, link, lang, assoc, date) "
+        + "VALUES (:title, :lines, :link, :lang, :assoc, :date)";
 
-		try {
+    try {
 
-			// Date entered by the user is parsed so that it conforms to the dd/MM/yy
-			// format.
-			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");
-			java.util.Date dateObj = format.parse(date);
-			// Convert date from (Java) Util to (Java) SQL object.
-			java.sql.Date sqlDate = new java.sql.Date(dateObj.getTime());
+      // Date entered by the user is parsed so that it conforms to the dd/MM/yy
+      // format.
+      SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");
+      java.util.Date dateObj = format.parse(date);
+      // Convert date from (Java) Util to (Java) SQL object.
+      java.sql.Date sqlDate = new java.sql.Date(dateObj.getTime());
 
-			/*
-			 * Here, the database is updated using the jdbcTemplate object. This is done by
-			 * passing the previously written SQL statement along with the map of parameters
-			 * to the update method.
-			 *
-			 * The key corresponds to the named parameters written after "VALUES" in the SQL
-			 * statement. The value corresponds to the actual values that will replace those
-			 * named parameters in the SQL query.
-			 */
-			MapSqlParameterSource params = new MapSqlParameterSource().addValue("title", title).addValue("lines", lines)
-					.addValue("link", link).addValue("lang", lang).addValue("assoc", assoc).addValue("date", sqlDate);
+      /*
+       * Here, the database is updated using the jdbcTemplate object. This is done by passing the
+       * previously written SQL statement along with the map of parameters to the update method.
+       *
+       * The key corresponds to the named parameters written after "VALUES" in the SQL statement.
+       * The value corresponds to the actual values that will replace those named parameters in the
+       * SQL query.
+       */
+      MapSqlParameterSource params = new MapSqlParameterSource().addValue("title", title)
+          .addValue("lines", lines).addValue("link", link).addValue("lang", lang)
+          .addValue("assoc", assoc).addValue("date", sqlDate);
 
-			jdbcTemplate.update(statement, params);
+      jdbcTemplate.update(statement, params);
 
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
 
-	}
+  }
 
-	/* Validates the title and link attributes of the form, preventing duplicates*/ 
-	public String validateTitleLink(String title, String link, String lang, String assoc) {
+  /* Validates the title and link attributes of the form, preventing duplicates */
+  public String validateTitleTypeLink(String title, String type, String link, String lang, String assoc) {
 
-		String UNIQUE_TITLE = "SELECT DISTINCT COUNT(title) FROM std_assets WHERE title = :title";
-		String UNIQUE_LINK = "SELECT DISTINCT COUNT(link) FROM std_assets WHERE link = :link";
+    String UNIQUE_TITLE = "SELECT DISTINCT COUNT(title) FROM std_assets WHERE title = :title";
+    String UNIQUE_LINK = "SELECT DISTINCT COUNT(link) FROM std_assets WHERE link = :link";
+    String UNIQUE_TYPE = "SELECT DISTINCT COUNT(type) FROM type WHERE name = :type";
 
-		try {
-			Map<String, String> parameters = new HashMap();
-			parameters.put("title", title);
 
-			int titleResult = (int) jdbcTemplate.queryForObject(UNIQUE_TITLE, parameters, Integer.class);
+    try {
+      Map<String, String> parameters = new HashMap();
+      parameters.put("title", title);
 
-			parameters.clear();
-			parameters.put("link", link);
+      int titleResult = (int) jdbcTemplate.queryForObject(UNIQUE_TITLE, parameters, Integer.class);
 
-			int linkResult = (int) jdbcTemplate.queryForObject(UNIQUE_LINK, parameters, Integer.class);
+      parameters.clear();
+      parameters.put("link", link);
 
-			if (titleResult != 0) {
-				throw new NotUnique("This title is not unique");
-			} else if (linkResult != 0) {
-				throw new NotUnique("This link is not unique");
-			}
+      int linkResult = (int) jdbcTemplate.queryForObject(UNIQUE_LINK, parameters, Integer.class);
+      
+      parameters.clear();
+      parameters.put("type", type);
 
-		} catch (NotUnique e) {
-			e.printStackTrace();
-			System.out.println("Error!");
-			return e.getMessage();
-		}
-		return "Form submitted successfully";
-	}
+      int typeResult = (int) jdbcTemplate.queryForObject(UNIQUE_TYPE, parameters, Integer.class);
 
-	 /* Validates the programming language selection attributes of the form, preventing unwanted results*/
-	public void validateSelection(String lang) throws InvalidSelection {
-		if (!programmingLanguagesSet.contains(lang)) {
-			throw new InvalidSelection("This programming language is not valid.");
-		}
-	}
+      if (titleResult != 0) {
+        throw new NotUnique("This title is not unique");
+      } else if (linkResult != 0) {
+        throw new NotUnique("This link is not unique");
+      } else if (typeResult != 0) {
+        throw new NotUnique("This type is not unique");
+      }
+
+    } catch (NotUnique e) {
+      e.printStackTrace();
+      System.out.println("Error!");
+      return e.getMessage();
+    }
+    return "Form submitted successfully";
+  }
+
+  /*
+   * Validates the programming language selection attributes of the form, preventing unwanted
+   * results
+   */
+  public void validateSelection(String lang) throws InvalidSelection {
+    if (!programmingLanguagesSet.contains(lang)) {
+      throw new InvalidSelection("This programming language is not valid.");
+    }
+  }
 
 }
