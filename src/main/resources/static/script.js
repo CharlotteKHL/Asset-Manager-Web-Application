a21405ae086428467a9efa893afd859bc2c884e5
Aryan Jolly <chestnutario389@gmail.com>
2024-02-09 19:44:23 +0000

    Updated JavaScript fetch method to appropriately display errors caught on server-side. Removed validation for 'type' selection field.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-// Client-side asset form validation - this is called upon form submission.
+// Client-side form validation - this method is called upon form submission.
 function validateEntries() {
 
     const assetForm = document.getElementById("assetForm");
@@ -15,6 +15,7 @@ function validateEntries() {
         alert.remove();
     });
 
+    // Boolean used to keep track of whether any alerts were triggered after all checks.
     let alertTriggered = false;
 
     /* The block of code below is used to generate and display the necessary (Bootstrap) alert.
@@ -35,6 +36,9 @@ function validateEntries() {
         placeholderElement.append(wrapper);
     }
 
+    /* N.B. All if statements below include an additional condition to check whether the alert already exists on the page.
+    This ensures that alerts do not stack i.e. pile up on the page. */
+
     // Checks whether the asset title is empty.
     if(entries[0].length == 0 && document.getElementById('titleAlert') === null) {
         alertTriggered = true;
@@ -47,38 +51,32 @@ function validateEntries() {
         appendAlert('You cannot enter a title longer than 50 characters!', 'danger', 'titleAlertPlaceholder', 'titleLengthAlert');
     }
 
-    // Checks whether the asset type has been left empty.
-    if(entries[1].length == 0 && document.getElementById('typeAlert') === null) {
-        alertTriggered = true;
-        appendAlert('You must enter an asset type!', 'danger', 'typeAlertPlaceholder', 'typeAlert');
-    }
-
     // Checks whether the number of lines has been left empty / is less than or equal to 0.
-    if((entries[2] == "" || entries[2] <= 0) && document.getElementById('noOfLinesAlert') === null) {
+    if((entries[1] == "" || entries[1] <= 0) && document.getElementById('noOfLinesAlert') === null) {
         alertTriggered = true;
         appendAlert('You must enter a positive number of lines!', 'danger', 'noOfLinesAlertPlaceholder', 'noOfLinesAlert');
     }
 
     // Checks whether the asset link is empty.
-    if(entries[3].length == 0 && document.getElementById('linkAlert') === null) {
+    if(entries[2].length == 0 && document.getElementById('linkAlert') === null) {
         alertTriggered = true;
         appendAlert('You must enter an asset link!', 'danger', 'linkAlertPlaceholder', 'linkAlert');
     }
 
     // Checks whether the programming language field is empty.
-    if(entries[4].length == 0 && document.getElementById('progLanguageAlert') === null) {
+    if(entries[3].length == 0 && document.getElementById('progLanguageAlert') === null) {
         alertTriggered = true;
         appendAlert('You must enter an asset programming language!', 'danger', 'progLanguageAlertPlaceholder', 'progLanguageAlert');
     }
 
     // Checks whether the associations field is empty.
-    if(entries[5].length == 0 && document.getElementById('associationsAlert') === null) {
+    if(entries[4].length == 0 && document.getElementById('associationsAlert') === null) {
         alertTriggered = true;
         appendAlert('You must enter asset association(s)!', 'danger', 'associationsAlertPlaceholder', 'associationsAlert');
     }
 
     // Checks whether date field has been left empty or does not conform to the date regular expression.
-    if((entries[6].length == 0 || !dateRegex.test(entries[6])) && document.getElementById('dateAlert') === null) {
+    if((entries[5].length == 0 || !dateRegex.test(entries[5])) && document.getElementById('dateAlert') === null) {
         alertTriggered = true;
         appendAlert('You must enter an asset creation date in the dd/mm/yyyy format!', 'danger', 'dateAlertPlaceholder', 'dateAlert');
     }
@@ -91,24 +89,32 @@ function validateEntries() {
         var formData = new FormData();
 
         formData.append("title", entries[0]);
-        // N.B. Type is skipped as this will be implemented later...
-        formData.append("lines", entries[2]);
-        formData.append("link", entries[3]);
-        formData.append("lang", entries[4]);
-        formData.append("assoc", entries[5]);
-        formData.append("date", entries[6]);
+        formData.append("lines", entries[1]);
+        formData.append("link", entries[2]);
+        formData.append("lang", entries[3]);
+        formData.append("assoc", entries[4]);
+        formData.append("date", entries[5]);
 
         fetch('/submit', {
             method: 'POST',
             body: formData,
         })
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return response.json().then(errorMessage => {
+                    throw new Error(errorMessage.error);
+                });
+            }
+        })
         .then(data => {
-            appendAlert('<i class="bi bi-check-circle-fill"></i>  Asset created successfully!', 'success', 'successAlertPlaceholder');
+            appendAlert('<i class="bi bi-check-circle-fill"></i> ' + data.message, 'success', 'successAlertPlaceholder');
         })
         .catch(error => {
-            appendAlert('<i class="bi bi-exclamation-triangle"></i>  Error: ' + error.message, 'danger', 'successAlertPlaceholder');
+            appendAlert('<i class="bi bi-exclamation-triangle"></i> Error: ' + error.message, 'danger', 'successAlertPlaceholder');
         });
+
     }
 
 }
