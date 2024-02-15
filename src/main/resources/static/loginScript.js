e55e61b63895c27fb202178c5fa6a3ae0ea36499
LeaneMatejko <zlac239@live.rhul.ac.uk>
2024-02-15 18:07:12 +0000

    Added call to check for back end validation.

--- src/main/resources/static/loginScript.js
+++ src/main/resources/static/loginScript.js
@@ -1,12 +1,54 @@
-const loginButton = document.getElementById('loginButton');
+function checkLogin() {
 
-let loginEmailEmpty = false;
+    const loginButton = document.getElementById('loginButton');
 
-// function called when the login button is pressed.
-loginButton.addEventListener("click", () => {
+    let loginEmailEmpty = true;
 
-    
-    
+    let valid = true;
+
+    alert("test1");
+
+    // The functions required to allow the alert(s) to appear above the form if input validation fails
+    const appendAlert = (message, type, placeholder) => {
+        const placeholderElement = document.getElementById(`${placeholder}`);
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">
+                <div>${message}</div>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close alert"></button>
+            </div>`
+        ].join('');
+
+        placeholderElement.append(wrapper);
+    }
+
+    alert("test2");
+
+    alert("test3");
+
+    if(valid){
+        var formData = new formData();
+
+        fetch('/submit', {
+            method: 'POST',
+            body: formData,
+        }).then(response => {
+            if(response.ok){
+                return response.json();
+            } else{
+                return response.json().then(errorMessage =>{
+                    throw new Error(errorMessage.error);
+                })
+            }
+        })
+        .then(data => {
+            appendAlert('<i class="bi bi-check-circle-fill"></i> ' + data.message, 'success', 'successAlertPlaceholder');
+        })
+        .catch(error => {
+            appendAlert('<i class="bi bi-exclamation-triangle"></i> Error: ' + error.message, 'danger', 'successAlertPlaceholder');
+        });
+    }
+
+    alert("test4");
+
 }
-
-)
