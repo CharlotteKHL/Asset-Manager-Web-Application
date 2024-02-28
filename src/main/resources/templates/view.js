bb7a2e957419ea1997a0c2995316a500a720579d
LeaneMatejko <zlac239@live.rhul.ac.uk>
2024-02-28 13:27:24 +0000

    Set up: Created inital post request for the asset on the view page.

--- src/main/resources/templates/view.js
+++ src/main/resources/templates/view.js
@@ -4,4 +4,22 @@ function replacePlaceholder() {
 
     assetName.innerText = "Test Asset";
     assetVariables.innerText = "Test Variable 1 \n Test variable 2."
+
+    var formData = new FormData();
+    formData.append("asset", document.getElementById("tempButton").value);
+
+    fetch('/view', {
+        method: 'POST',
+        body: formData
+
+    }).then(res => {
+        if(res.ok){
+            console.log('Success');
+        }else{
+            console.log('Error');
+        }
+    }).then( 
+        data => console.log(data)
+    );
+
 }
