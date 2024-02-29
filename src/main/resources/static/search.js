dba48753cde62e347f244377fa209672f943c645
Alexandra D <zlac480@live.rhul.ac.uk>
2024-02-29 12:38:20 +0000

    added a submit button for the filters. added in a table to display the assets from the search and filters

--- src/main/resources/static/search.js
+++ src/main/resources/static/search.js
@@ -1,5 +1,8 @@
 function fetchAssets() {
     let assetToQuery = document.getElementById("searchField").value;
+    
+    
+    
     console.log("Look for an asset that has '" + assetToQuery + "' in its name, as well as the applied filters.");
     console.log("For no / empty filters, simply list all assets.");
     console.log("For no results, display a message accordingly.");
