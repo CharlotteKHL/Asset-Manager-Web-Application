f5bbe2f10b11e0f57ac243298881080d71230cc3
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2024-02-29 14:41:44 +0000

    Main file updated to match main branch.

--- src/main/java/com/team05/assetsrepo/Main.java
+++ src/main/java/com/team05/assetsrepo/Main.java
@@ -2,17 +2,11 @@ package com.team05.assetsrepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Main class used to start the application.
  */
 @SpringBootApplication
-@EnableJpaRepositories("com.team05.assetsrepo.*")
-@ComponentScan(basePackages = { "com.team05.assetsrepo" })
-@EntityScan("com.team05.assetsrepo.*")
 public class Main {
 
   public static void main(String[] args) {
