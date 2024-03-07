38813c96a1761c7c5e2a25f895ae4d86f9bde805
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2024-03-07 12:38:00 +0000

    Setup configuration for springboot sessions with jdbc.

--- src/main/java/com/team05/assetsrepo/SecurityConfig.java
+++ src/main/java/com/team05/assetsrepo/SecurityConfig.java
@@ -10,8 +10,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -50,8 +53,18 @@ public class SecurityConfig {
     @Autowired
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request.anyRequest().permitAll()).csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated()).csrf(AbstractHttpConfigurer::disable);
         http.formLogin(form -> form.loginPage("/login.html").permitAll());
         return http.build();
+    }
+    
+    @Autowired
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("admin")
+            .password(passwordEncoder.encode("password"))
+            //.roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
