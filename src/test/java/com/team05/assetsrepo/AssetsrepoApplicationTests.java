34db943831a95619bf40d6165f8a73ae7d31d928
LeaneMatejko <zlac239@live.rhul.ac.uk>
2024-03-11 14:28:30 +0000

    Setup: Updated testing template to be compatible with JUnit5.

--- src/test/java/com/team05/assetsrepo/AssetsrepoApplicationTests.java
+++ src/test/java/com/team05/assetsrepo/AssetsrepoApplicationTests.java
@@ -2,21 +2,39 @@ package com.team05.assetsrepo;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AssetsrepoApplicationTests {
 
   
   @Autowired
-  private FormController controller;
+  private FormController mockMVC;
+  
+  @MockBean
+  private final NamedParameterJdbcTemplate jdbcTemplate = null;
+  
+//  @BeforeAll
+//  void setUp() {
+//	  this.jdbcTemplate = 
+//  }
 
   //checks to see if the FormController Exists
   @Test
   void contextLoads() {
-      assertNotEquals(controller, null);
+      assertNotEquals(mockMVC, null);
   }
 
 }
