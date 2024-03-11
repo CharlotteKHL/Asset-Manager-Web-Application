40d2e0331b65726550cbc71c216affb19badaa03
LeaneMatejko <zlac239@live.rhul.ac.uk>
2024-03-11 14:42:26 +0000

    SetUp: Set up test class fro AccountController.

---
+++ src/test/java/com/team05/assetsrepo/FormControllerTests.java
@@ -0,0 +1,36 @@
+package com.team05.assetsrepo;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class FormControllerTests {
+
+  
+  @Autowired
+  private FormController mockMVC;
+  
+  @MockBean
+  private final NamedParameterJdbcTemplate jdbcTemplate = null;
+
+  //checks to see if the FormController Exists
+  @Test
+  void contextLoads() {
+      assertNotEquals(mockMVC, null);
+  }
+  
+
+}
