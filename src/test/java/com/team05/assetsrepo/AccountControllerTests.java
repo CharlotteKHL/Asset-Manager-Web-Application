522e59c594e205eadc630b1c8673b4e15ce83be0
LeaneMatejko <zlac239@live.rhul.ac.uk>
2024-03-11 16:25:09 +0000

    Tests: Added initial tests for the AccountController.

--- src/test/java/com/team05/assetsrepo/AccountControllerTests.java
+++ src/test/java/com/team05/assetsrepo/AccountControllerTests.java
@@ -1,13 +1,14 @@
 package com.team05.assetsrepo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -17,15 +18,50 @@ class AccountControllerTests {
   
   @Autowired
   private AccountController mockMVC;
-  
-  @MockBean
-  private final NamedParameterJdbcTemplate jdbcTemplate = null;
 
   //checks to see if the FormController Exists
   @Test
   void contextLoads() {
       assertNotEquals(mockMVC, null);
+      System.out.println(mockMVC);
   }
   
+  //Testing the extractLogin method (with static variables)
+  @Test
+  void testExtractLoginTry() throws InvalidLogin {
+	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Login successful\"}")), mockMVC.extractLogin("hi@gmail.com", "hi"));
+  }
+  
+  //Testing the try block of the validateLoginDetails method.
+  @Test
+  void validateLoginDetailsTry() {
+	  assertEquals(mockMVC.validateLoginDetails("hi@gmail.com", "hi"), "Login successful");
+	  
+  }
+  
+////Testing for the invalid Login catch block from the validateLoginDetails method.
+//@Test
+//void testValidateLoginDetailsCatch() throws InvalidLogin {
+//	  InvalidLogin errorMessage = assertThrows(InvalidLogin.class, () -> mockMVC.extractLogin("wrongUsername@Gmail.com", "wrongPassword"));
+//	  assertEquals("This password is not correct", errorMessage);
+//}
+  
+  //Testing the getPassword method.
+  @Test
+  void testGetPassword() {
+	  assertEquals("hi", mockMVC.getPassword("hi@gmail.com"));
+  }
+  
+  //Testing the getPassword method.
+  @Test
+  void testRegisterUniqueUsername() {
+	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Registration successful\"}")), mockMVC.register("tested@Gmail.com", "testedData"));
+  }
+  
+  //Testing the getPassword method.
+  @Test
+  void testRegisterNotUniqueUsername() {
+	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Registration unsuccessful, this email may already have an account\"}")), mockMVC.register("hi@gmail.com", "testedData"));
+  }
 
 }
--- src/test/java/com/team05/assetsrepo/FormControllerTests.java
+++ src/test/java/com/team05/assetsrepo/FormControllerTests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
