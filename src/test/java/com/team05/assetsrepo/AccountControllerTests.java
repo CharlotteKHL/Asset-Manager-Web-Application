5617a627a887ee62fe8371bc402e8db70b65f950
LeaneMatejko <zlac239@live.rhul.ac.uk>
2024-03-12 12:29:16 +0000

    Setup: Updated some tests for Accoutn Controller.

--- src/test/java/com/team05/assetsrepo/AccountControllerTests.java
+++ src/test/java/com/team05/assetsrepo/AccountControllerTests.java
@@ -2,6 +2,7 @@ package com.team05.assetsrepo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -10,58 +11,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AccountControllerTests {
-
   
   @Autowired
-  private AccountController mockMVC;
+  private AccountController accountController;
 
   //checks to see if the FormController Exists
   @Test
   void contextLoads() {
-      assertNotEquals(mockMVC, null);
-      System.out.println(mockMVC);
+      assertNotEquals(accountController, null);
+      System.out.println(accountController);
   }
   
   //Testing the extractLogin method (with static variables)
   @Test
   void testExtractLoginTry() throws InvalidLogin {
-	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Login successful\"}")), mockMVC.extractLogin("hi@gmail.com", "hi"));
+	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Login successful\"}")), accountController.extractLogin("hi@gmail.com", "hi"));
   }
   
   //Testing the try block of the validateLoginDetails method.
   @Test
   void validateLoginDetailsTry() {
-	  assertEquals(mockMVC.validateLoginDetails("hi@gmail.com", "hi"), "Login successful");
+	  assertEquals(accountController.validateLoginDetails("hi@gmail.com", "hi"), "Login successful");
 	  
   }
   
 ////Testing for the invalid Login catch block from the validateLoginDetails method.
 //@Test
-//void testValidateLoginDetailsCatch() throws InvalidLogin {
-//	  InvalidLogin errorMessage = assertThrows(InvalidLogin.class, () -> mockMVC.extractLogin("wrongUsername@Gmail.com", "wrongPassword"));
-//	  assertEquals("This password is not correct", errorMessage);
+//void testValidateLoginDetailsCatch(){
+//	  assertThrows(InvalidLogin.class, () -> accountController.extractLogin("wrongUsername@Gmail.com", "wrongPassword"));
 //}
   
   //Testing the getPassword method.
   @Test
   void testGetPassword() {
-	  assertEquals("hi", mockMVC.getPassword("hi@gmail.com"));
+	  assertEquals("hi", accountController.getPassword("hi@gmail.com"));
   }
   
   //Testing the getPassword method.
   @Test
   void testRegisterUniqueUsername() {
-	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Registration successful\"}")), mockMVC.register("tested@Gmail.com", "testedData"));
+	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Registration successful\"}")), accountController.register("testedData@gmail.com", "testedData"));
   }
   
   //Testing the getPassword method.
   @Test
   void testRegisterNotUniqueUsername() {
-	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Registration unsuccessful, this email may already have an account\"}")), mockMVC.register("hi@gmail.com", "testedData"));
+	  assertEquals((ResponseEntity.ok().body("{\"message\": \"Registration unsuccessful, this email may already have an account\"}")), accountController.register("hi@gmail.com", "testedData"));
   }
 
 }
