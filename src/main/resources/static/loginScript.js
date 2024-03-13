75114a1c5043ddf8c1d956155c7fe793456fb47a
Daniel Bee <ZLAC198@live.rhul.ac.uk>
2024-03-13 14:32:30 +0000

    Implemented first part of showing username on the front-end

--- src/main/resources/static/loginScript.js
+++ src/main/resources/static/loginScript.js
@@ -76,3 +76,29 @@ function sleep(ms) {
     clearInterval(sleepInterval);
     return new Promise(resolve => sleepInterval = setTimeout(resolve, ms));
 }
+
+// When the page is started up, a check is carried out to confirm if the user is logged in or not
+// If the user is logged in, then their username is displayed with a welcome message
+// If the user is not logged in, then the Login/Register buttons are displayed instead
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/', {
+        method: 'POST'
+    }).then(response => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            return response.json().then(errorMessage => {
+                throw new Error(errorMessage.error);
+            })
+        }
+    }).then(data => {
+        if(data.username != null) {
+            var loginRegisterButtons = document.getElementById('login-register-buttons');
+            loginRegisterButtons.style.display = 'none';
+            var welcomeUserMessage = document.getElementById('welcome-user-message');
+            welcomeUserMessage.style.display = 'block';
+            var usernameText = document.getElementById('username');
+            usernameText.innerHTML = data.username;
+        }
+    })
+});
