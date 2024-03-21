2d4a35548831c1a72c589cf594b56d7edaf3571c
Daljit Singh Dhillon <ZEAC037@live.rhul.ac.uk>
2024-03-21 21:30:24 +0000

    Refactor: fixed typo in permit all section.
    Needed 2 X * rather than the single one.

--- src/main/java/com/team05/assetsrepo/SecurityConfig.java
+++ src/main/java/com/team05/assetsrepo/SecurityConfig.java
@@ -19,63 +19,57 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@ComponentScan(basePackages = {"com.team05.assetsrepo"})
+@ComponentScan(basePackages = { "com.team05.assetsrepo" })
 @EnableWebSecurity
 public class SecurityConfig {
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  @Autowired
-  @Bean
-  public CustomUserDetailsService customUserDetailsService() {
-    return new CustomUserDetailsService(userRepository);
-  }
+    @Autowired
+    @Bean
+    public CustomUserDetailsService customUserDetailsService() {
+        return new CustomUserDetailsService(userRepository);
+    }
 
-  @Autowired
-  public PasswordEncoder encoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Autowired
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Autowired
-  @Bean
-  public DaoAuthenticationProvider authProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(customUserDetailsService());
-    authProvider.setPasswordEncoder(encoder());
-    return authProvider;
-  }
+    @Autowired
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customUserDetailsService());
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
 
-  @Autowired
-  @Bean
-  public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-    return http.getSharedObject(AuthenticationManagerBuilder.class)
-        .authenticationProvider(authProvider()).build();
-  }
+    @Autowired
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).authenticationProvider(authProvider()).build();
+    }
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.formLogin(formLogin -> formLogin.loginPage("/login.html").defaultSuccessUrl("/", true)
-        .usernameParameter("exampleInputEmail").passwordParameter("exampleInputPassword"));
-    http.authorizeHttpRequests(
-        request -> request.requestMatchers(new AntPathRequestMatcher("/")).hasRole("ADMIN"));
-    http.authorizeHttpRequests(request -> request.requestMatchers("/*").permitAll())
-        .csrf(AbstractHttpConfigurer::disable);
-    http.authorizeHttpRequests(request -> request
-        .requestMatchers("/checkSession/**", "/loginScript/**", "/registerScript/**", "/script/**")
-        .permitAll());
-    http.logout(logout -> logout.logoutSuccessUrl("/logout").deleteCookies("SESSION").permitAll());
-    http.headers(header -> header.frameOptions(
-        frameOptions -> frameOptions.disable().contentTypeOptions(cto -> cto.disable())));
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(formLogin -> formLogin.loginPage("/login.html").defaultSuccessUrl("/", true)
+            .usernameParameter("exampleInputEmail").passwordParameter("exampleInputPassword"));
+        http.authorizeHttpRequests(request -> request.requestMatchers(new AntPathRequestMatcher("/")).hasRole("ADMIN"));
+        http.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll()).csrf(AbstractHttpConfigurer::disable);
+        http.logout(logout -> logout.logoutSuccessUrl("/logout").deleteCookies("SESSION").permitAll());
+        http.headers(header -> header.frameOptions(frameOptions -> frameOptions.disable().contentTypeOptions(cto -> cto.disable())));
+        return http.build();
+    }
 
-  @Autowired
-  @Bean
-  public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-    UserDetails user = User.withUsername("hi2@gmail.com").password(passwordEncoder.encode("hi"))
-        // .roles("ADMIN")
-        .build();
-    return new InMemoryUserDetailsManager(user);
-  }
-
+    @Autowired
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+      UserDetails user = User.withUsername("hi2@gmail.com")
+          .password(passwordEncoder.encode("hi"))
+          //.roles("ADMIN")
+          .build();
+      return new InMemoryUserDetailsManager(user);
+    }
+    
 }
